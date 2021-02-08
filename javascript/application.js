b57743607ec9063ab12774dbe7d26c6afd891d71
raphael allard <raphael.s.allard@gmail.com>
2021-02-08 19:16:56 +0100

    added js functions to navbar

--- javascript/application.js
+++ javascript/application.js
@@ -21,20 +21,24 @@ const displayNavbarMenuOnClick = () => {
 // Change the color of the navbar on scroll:
 const changeDesignNavbarOnScroll = () => {
   const navbar = document.querySelector('nav');
-  // const h1PositionY = document.querySelector('h1').getBoundingClientRect().top;
   const navbarHeight = navbar.offsetHeight;
-  const scrollPositionY = window.scrollY;
+  const img = document.querySelector('.navbar-box img');
+  const navbarListItems = document.querySelectorAll('.navbar-list-box a');
   
   window.addEventListener('scroll', (event) => {
-    // console.log(navbar.offsetHeight);
-    if (scrollPositionY > navbarHeight) {
-      console.log('hello');
-    }
+    const scrollPositionY = window.scrollY;
     
+    if (scrollPositionY >= navbarHeight - 10) {
+      navbar.classList.add('active');
+      img.src = 'images/allard-prod-white.png';
+      navbarListItems.forEach(item => item.classList.add('active'));
+    } else {
+      navbar.classList.remove('active');
+      img.src = 'images/allard-prod-black.png';
+      navbarListItems.forEach(item => item.classList.remove('active'));
+    }
   });
 };
 
-
- 
 displayNavbarMenuOnClick();
 changeDesignNavbarOnScroll();
