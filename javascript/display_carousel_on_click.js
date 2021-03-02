9e53e9eefe3da3601f37834f7d9c847bdb257c19
raphael allard <raphael.s.allard@gmail.com>
2021-03-02 17:24:25 +0100

    quick fixs & design improved

--- javascript/display_carousel_on_click.js
+++ javascript/display_carousel_on_click.js
@@ -2,17 +2,19 @@ const displayCarouselOnClick = () => {
   const cards = document.querySelectorAll('.card');
 
   cards.forEach((card) => {
-    const plusIcon = card.querySelector('.fa-plus-circle');
+    const plusIcon = card.querySelector('.fa-camera-retro');
     const projectDescriptionHover = card.querySelector('.card-hover-div');
     const projectCarousel = card.querySelector('.carousel');
 
     plusIcon.addEventListener('click', (event) => {
       projectDescriptionHover.classList.add('d-none');
       projectCarousel.classList.remove('d-none');
+      card.classList.add('border-0');
       
       card.addEventListener('mouseleave', (event) => {
         projectDescriptionHover.classList.remove('d-none');
         projectCarousel.classList.add('d-none');
+        card.classList.remove('border-0');
       })
     });
   });
