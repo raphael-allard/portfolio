d5b19b06e009519fe6f9365fc01cc522814b5ca6
raphael allard <raphael.s.allard@gmail.com>
2021-03-05 19:20:04 +0100

    sections til projects done

--- javascript/change_design_navbar_on_scroll.js
+++ javascript/change_design_navbar_on_scroll.js
@@ -3,6 +3,7 @@ const changeDesignNavbarOnScroll = () => {
   const navbarHeight = navbar.offsetHeight;
   const img = document.querySelector('.navbar-box img');
   const navbarListItems = document.querySelectorAll('.navbar-list-box a');
+  const navbarListItemsMDScreen = document.querySelectorAll('.navbar-list-box-md-screen h3');
   
   window.addEventListener('scroll', (event) => {
     const scrollPositionY = window.scrollY;
@@ -11,10 +12,12 @@ const changeDesignNavbarOnScroll = () => {
       navbar.classList.add('active');
       img.src = 'images/allard-prod-white.png';
       navbarListItems.forEach(item => item.classList.add('active'));
+      navbarListItemsMDScreen.forEach(item => item.style.color = "white");
     } else {
       navbar.classList.remove('active');
       img.src = 'images/allard-prod-black.png';
       navbarListItems.forEach(item => item.classList.remove('active'));
+      navbarListItemsMDScreen.forEach(item => item.style.color = "#1f1f1f");
     }
   });
 };
