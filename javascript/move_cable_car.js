989675b41a5be1950f3be801c18c9ff21b464f62
raphael allard <raphael.s.allard@gmail.com>
2021-03-07 19:20:17 +0100

    delete a console.log

--- javascript/move_cable_car.js
+++ javascript/move_cable_car.js
@@ -1,7 +1,6 @@
 const moveCableCar = () => {
   const cableCar = document.querySelector('.fa-tram');
   const leftValue = getComputedStyle(cableCar).left;
-  console.log(leftValue);
 
   if (leftValue === "0px") {
     cableCar.style.left = "94%";
