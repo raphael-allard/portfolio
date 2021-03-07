32b09d0736f38b6b768add3465e6e9b112f67549
raphael allard <raphael.s.allard@gmail.com>
2021-03-07 18:39:53 +0100

    cable car feature implemented

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,13 @@
 import { displayNavbarMenuOnClick } from './display_navbar_menu_on_click.js';
 import { changeDesignNavbarOnScroll } from './change_design_navbar_on_scroll.js';
 import { toggleWindButtonOnClick } from './toggle_wind_button_on_click.js';
+import { moveCableCar } from './move_cable_car.js';
 import { displayCarouselOnClick } from './display_carousel_on_click.js';
 import { displaySweetAlertOnClick } from './display_sweet_alert_on_click.js';
 
 displayNavbarMenuOnClick();
 changeDesignNavbarOnScroll();
 toggleWindButtonOnClick();
+moveCableCar();
 displayCarouselOnClick();
 displaySweetAlertOnClick();
